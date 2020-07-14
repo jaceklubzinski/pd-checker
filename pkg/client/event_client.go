b17a49f88914a8a6c0c0a066f14cfa6fba4c628b
Jacek Lubziński <jacek@piwik.pro>
2020-07-14 08:22:05 +0200

    fix

--- pkg/client/event_client.go
+++ pkg/client/event_client.go
@@ -4,12 +4,20 @@ import (
 	"github.com/PagerDuty/go-pagerduty"
 )
 
+type EventClient struct {
+	client *pagerduty.V2Event
+}
+
+func NewEventClient(client *pagerduty.V2Event) *EventClient {
+	return &EventClient{client: client}
+}
+
 type Manager interface {
-	ManageEvent(options *pagerduty.V2Event) (*pagerduty.V2EventResponse, error)
+	ManageEvent() (*pagerduty.V2EventResponse, error)
 }
 
 //ManageEvent create or resolve event using PagerDuty API
-func ManageEvent(options *pagerduty.V2Event) (*pagerduty.V2EventResponse, error) {
-	response, error := pagerduty.ManageEvent(*options)
+func (c *EventClient) ManageEvent() (*pagerduty.V2EventResponse, error) {
+	response, error := pagerduty.ManageEvent(*c.client)
 	return response, error
 }
--- pkg/event/event_manage_test.go
+++ pkg/event/event_manage_test.go
@@ -11,10 +11,10 @@ type mockEvent struct {
 }
 
 type mockEventManager interface {
-	ManageEvent(options *pagerduty.V2Event) (*pagerduty.V2EventResponse, error)
+	ManageEvent() (*pagerduty.V2EventResponse, error)
 }
 
-func (m *mockEvent) ManageEvent(options *pagerduty.V2Event) (*pagerduty.V2EventResponse, error) {
+func (m *mockEvent) ManageEvent() (*pagerduty.V2EventResponse, error) {
 	response := pagerduty.V2EventResponse{
 		Status: "success",
 	}
@@ -25,7 +25,7 @@ func TestManageIncident(t *testing.T) {
 	var opts pagerduty.V2Event
 	clientEvent := Event{
 		Options: &opts,
-		manager: client,
+		Manager: client,
 	}
 	clientEvent.NewRecordMetricsEvent()
 	clientEvent.SetPayLoad("24h")
